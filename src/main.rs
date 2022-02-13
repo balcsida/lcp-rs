c6a03c6b8bd7ec33471cabdafe50006d633d5af9
Dávid Balatoni <balcsida@gmail.com>
2022-02-13 13:08:23 +0000

    chore!: Switch to Clap v3

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,46 @@
-#[macro_use]
-extern crate ini;
-extern crate dirs;
-use std::path::Path;
-extern crate clap;
-use clap::{crate_authors, crate_description, crate_version, App, Arg, SubCommand};
+use clap::{AppSettings, Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(global_setting(AppSettings::PropagateVersion))]
+#[clap(global_setting(AppSettings::UseLongFormatForHelpSubcommand))]
+#[clap(global_setting(AppSettings::SubcommandRequiredElseHelp))]
+#[clap(global_setting(AppSettings::DeriveDisplayOrder))]
+#[clap(global_setting(AppSettings::DisableHelpSubcommand))]
+
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Delete service(s)
+    Delete {
+        /// Name of the project to delete
+        #[clap(short, long)]
+        project: String,
+        /// Environment name to delete (e.g. "dev")
+        #[clap(short, long)]
+        environment: String,
+        /// Service name (e.g. "liferay")
+        #[clap(short, long)]
+        service: Option<String>,
+    },
+}
 
 fn main() {
-    let matches = App::new("Alternative Liferay Cloud Platform CLI (alcp)")
-        .version(crate_version!())
-        .author(crate_authors!())
-        .about(crate_description!())
-        .arg(
-            Arg::with_name("config")
-                .short("c")
-                .long("config")
-                .value_name("FILE")
-                .help("Sets a custom config file")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("v")
-                .short("v")
-                .multiple(true)
-                .help("Sets the level of verbosity"),
-        )
-        .subcommand(
-            SubCommand::with_name("delete")
-                .about("Deletes service(s)")
-                .arg(
-                    Arg::with_name("debug")
-                        .short("d")
-                        .help("print debug information verbosely"),
-                ),
-        )
-        .get_matches();
-    // Gets a value for config if supplied by user, or defaults to "default.conf"
-    let config = matches.value_of("config").unwrap_or("default.conf");
-    println!("Value for config: {}", config);
+    let cli = Cli::parse();
 
-    let path = Path::new(dirs::home_dir().unwrap().to_str().unwrap()).join(".lcp");
-    let map = ini!(path.to_str().unwrap());
-    // Proceed to use normal HashMap functions on the map:
-    let lcp_token = map["remote \"lcp\""]["token"].clone().unwrap();
-    println!("{}", lcp_token);
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level app
+    match &cli.command {
+        Commands::Delete { project, environment, service } => {
+            println!("'alcp delete' was used");
 
-    // You can handle information about subcommands by requesting their matches by name
-    // (as below), requesting just the name used, or both at the same time
-    if let Some(matches) = matches.subcommand_matches("delete") {
-        if matches.is_present("debug") {
-            println!("Printing debug info...");
-        } else {
-            println!("Printing normally...");
+            println!("Project is: {:?}", project);
+            println!("Environment is: {:?}", environment);
+            println!("Service is: {:?}", service)
         }
     }
 }
